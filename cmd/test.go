83332af33b452571471331d4a34e6975741a278e
J-wyt <546108612@qq.com>
2021-04-25 11:30:23 +0800

    add RouterGroup

--- cmd/test.go
+++ cmd/test.go
@@ -7,23 +7,34 @@ import (
 
 func main() {
 	r := girov.New()
-	r.GET("/", func(c *girov.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *girov.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *girov.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(c *girov.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *girov.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *girov.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *girov.Context) {
+			c.JSON(http.StatusOK, girov.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *girov.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *girov.Context) {
-		c.JSON(http.StatusOK, girov.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
--- girov.go
+++ girov.go
@@ -1,6 +1,7 @@
 package girov
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -17,28 +18,60 @@ type Engine struct {
 	// 路由映射表router
 	// key由请求方法和静态路由地址构成，例如GET-/、GET-/hello、POST-/hello
 	// 这样针对相同的路由，如果请求方法不同,可以映射不同的处理方法(Handler)，value 是用户映射的处理方法
-	//router map[string]HandlerFunc
 	router *router
+	// 将 Engine 作为最顶层的分组，也就是说 Engine 拥有 RouterGroup 所有的能力
+	*RouterGroup
+	groups []*RouterGroup // store all groups
+}
+
+// RouterGroup 分组控制(Group Control)是 Web 框架应提供的基础功能之一。
+// 所谓分组，是指路由的分组。如果没有路由分组，我们需要针对每一个路由进行控制。但是真实的业务场景中，往往某一组路由需要相似的处理
+// 一个 Group 对象需要具备哪些属性呢？
+// 首先是前缀(prefix)，比如/，或者/api
+// 要支持分组嵌套，那么需要知道当前分组的父亲(parent)是谁
+// 当然了，按照我们一开始的分析，中间件是应用在分组上的，那还需要存储应用在该分组上的中间件(middlewares)
+// 还需要有访问Router的能力，为了方便，我们可以在Group中，保存一个指针，指向Engine，整个框架的所有资源都是由Engine统一协调的，那么就可以通过Engine间接地访问各种接口了
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	engine      *Engine       // all groups share a Engine instance
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
 // addRoute 增加路由
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 // Run defines the method to start a http server
