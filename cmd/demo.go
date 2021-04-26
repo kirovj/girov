c54dc696ee1d0fdcce6c06069c749820ce0b0d16
J-wyt <546108612@qq.com>
2021-04-26 18:59:32 +0800

    day7 panic

--- cmd/demo.go
+++ cmd/demo.go
@@ -2,47 +2,19 @@ package main
 
 import (
 	"girov"
-	"log"
 	"net/http"
-	"time"
 )
-
-func onlyForV2() girov.HandlerFunc {
-	return func(c *girov.Context) {
-		// Start timer
-		t := time.Now()
-		// if a server error occurred
-		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
-}
 
 func main() {
 	r := girov.New()
-	r.Use(girov.Logger()) // global middleware
-	r.GET("/index", func(c *girov.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.GET("/", func(c *girov.Context) {
+		c.String(http.StatusOK, "Hello Geektutu\n")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *girov.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-		})
-
-		v1.GET("/hello", func(c *girov.Context) {
-			// expect /hello?name=geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *girov.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-	}
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *girov.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
+	})
 
 	r.Run(":9999")
 }
